6f410f5d317ba1a015fb2edaf48dfa08ef22bde4
wiem sghaier <wiemsghaierr@gmail.com>
2021-10-29 11:26:28 +0200

    final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chat-app
+Apache Kafka chat app
---
+++ src/main/java/com/messageapp/kafkachat/constants/KafkaConstants.java
@@ -0,0 +1,9 @@
+package com.messageapp.kafkachat.constants;
+
+public class KafkaConstants {
+
+        public static final String KAFKA_TOPIC = "kafka-chat";
+        public static final String GROUP_ID = "kafka-sandbox";
+        public static final String KAFKA_BROKER = "localhost:9092";
+
+}
